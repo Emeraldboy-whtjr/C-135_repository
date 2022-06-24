eb5f76c548e02adc0a312a4898bb7e72949debdd
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2022-06-24 17:00:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var status = "";
+var object = "";
+
 function preload(){
 
 }
@@ -8,10 +11,23 @@ function setup(){
     
     video = createCapture(VIDEO);
     video.hide();
+    video.size(600,400);
 }
 
 function draw(){
 
     image(video,0,0,600,400)
 
+}
+
+function start(){
+    object_detection = objectDetector("cocossd",model_loaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+    object = document.getElementById("input").value;
+
+}
+
+function model_loaded(){
+    console.log("model has been loaded");
+    status = true;
 }
